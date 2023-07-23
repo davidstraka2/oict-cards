4ef3ada4ef2008a9a3898830a055394789c8493a
David Straka <mail@davidstraka.dev>
2023-07-23 13:23:45 +0000

    Set up Containerfile and Compose file

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import express, {Request, Response} from 'express';
 
 const app = express();
 const port = 3000;
+const host = '0.0.0.0';
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => {
+app.listen(port, host, () => {
     console.log(`Example app listening on port ${port}`);
 });
